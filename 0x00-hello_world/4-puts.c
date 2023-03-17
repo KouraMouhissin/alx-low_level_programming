ce5ae73c1012e32343fcbacbf1be83c1cdfcb6a5
KouraMouhissin <mouhissin@gmail.com>
2023-03-17 08:45:36 -0700

    betty style again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,12 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
- * Description: 'the program's description'
- * @parameter: describe the parameter
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 char Mystr[] = "\"Programming is like building a multilingual puzzle";
-puts(Mystr);/*using puts to print my string*/
+puts(Mystr); /*using puts to print my string*/
 return (0);
 }
