50da9891dd7e2746b6817671807edcb1a816ebee
KouraMouhissin <mouhissin@gmail.com>
2023-03-17 08:26:33 -0700

    4-puts after betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program's description'
+ * @parameter: describe the parameter
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-char Mystr[] = "\"Programming is like building a multilingual puzzle"; //declaring my string
-puts(Mystr); //using puts to print my string
+char Mystr[] = "\"Programming is like building a multilingual puzzle";
+puts(Mystr);/*using puts to print my string*/
 return (0);
 }
