e37d10f53740adc550ed72fd8fab640e784d1fbf
KouraMouhissin <mouhissin@gmail.com>
2023-03-17 09:13:54 -0700

    the s added to the output

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,9 +13,9 @@ float floatType;
 char charType;
 /*printing sizes*/
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
-printf("Size of an int: %zu byte (s)\n", sizeof(intType));
-printf("Size of long int: %zu bytes\n", sizeof(longType));
-printf("Size of long long int: %zu bytes\n", sizeof(longgType));
-printf("Size of float: %zu byte (s)\n", sizeof(floatType));
+printf("Size of an int: %zu byte(s)\n", sizeof(intType));
+printf("Size of long int: %zu byte(s)\n", sizeof(longType));
+printf("Size of long long int: %zu byte(s)\n", sizeof(longgType));
+printf("Size of float: %zu byte(s)\n", sizeof(floatType));
 return (0);
 }
