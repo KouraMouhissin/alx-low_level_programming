1deaff2476c2c732d1254b2c46d4f40a4f938c1c
KouraMouhissin <mouhissin@gmail.com>
2023-03-21 11:18:29 -0700

    1198

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ void print_to_98(int n)
 {
 	if (n > 99)
 	{
-	while (n > 99)
+	while (n >= 98)
 	{
 		if (n == 98)
 		{
@@ -26,7 +26,7 @@ void print_to_98(int n)
 	}
 	else if (n < 99)
 	{
-	while (n < 99)
+	while (n <= 98)
 	{
 		if (n == 98)
 		{
