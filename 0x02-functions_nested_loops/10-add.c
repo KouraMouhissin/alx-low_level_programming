057292088b205a0bd1053d751ebc42d662b9980b
KouraMouhissin <mouhissin@gmail.com>
2023-03-21 10:17:45 -0700

    adds

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ ** add - Entry point
+ ** @a: parameter
+ ** @b: parameter
+ ** Return: Always 0 (Success)
+ **/
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
