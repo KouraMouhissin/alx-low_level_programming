2c1f96887a9b5401cf68e21144728e693a302633
KouraMouhissin <mouhissin@gmail.com>
2023-03-23 10:19:07 -0700

    print most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ ** print_most_numbers - Entry point
+ ** Return: Always 0 (Success)
+ **/
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c == 50 || c == 52)
+		{
+		}
+		else
+		{
+		putchar(c);
+		}
+	}
+	putchar('\n');
+}
