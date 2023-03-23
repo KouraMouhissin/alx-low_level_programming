bd424a2e29c1bd8bb3d8826cf7d5e83c2284c10f
KouraMouhissin <mouhissin@gmail.com>
2023-03-23 07:31:21 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ ** _isdigit - Entry point
+ ** @c: parameter
+ ** Return: Always 0 (Success)
+ **/
+
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
