ad95d372039ac2bdd9e9b7ae757509bf8022bd66
KouraMouhissin <mouhissin@gmail.com>
2023-03-23 07:19:22 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ ** _isdigit - Entry point
+ ** @c: parameter
+ ** Return: Always 0 (Success)
+ **/
+
+int _isdigit(int c)
+{
+	if (x >= 46 && x <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
