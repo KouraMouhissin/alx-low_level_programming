5cb712d0aa726e60238413ff2d6d0176aa0ef280
KouraMouhissin <mouhissin@gmail.com>
2023-03-23 09:39:05 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ ** mul - Entry point
+ ** @a: parameter
+ ** @b: parameterr
+ ** Return: Always 0 (Success)
+ **/
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
