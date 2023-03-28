4317095e50c3e0197002a8ca532203e0c79a4d75
KouraMouhissin <mouhissin@gmail.com>
2023-03-28 05:04:15 -0700

    print rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,5 +8,15 @@
 
 void print_rev(char *s)
 {
-	
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (s[count] == '\0')
+		break;
+	count++;
+	}
+	for (count--; count >= 0; count--)
+		putchar(s[count]);
+	putchar('\n');
 }
