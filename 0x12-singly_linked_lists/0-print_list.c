9d4e71b14d058f49db610c85f8524f0ac777b93a
KouraMouhissin <mouhissin@gmail.com>
2023-04-27 10:19:17 -0700

    First commit of tasks 0, 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+/**
+ * print_list - print elements in a linked list
+ * @h: pointer to first node
+ * Return: nodes numbers
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t node_nbr = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+
+		h = h->next;
+		node_nbr += 1;
+	}
+	
+	printf("[%d] %s\n", h->len, h->str);
+	return (node_nbr);
+}
+
---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * list_len - number of nodes
+ * @h: pointer to the first node
+ * Return: nodes nbr
+*/
+
+size_t list_len(const list_t *h)
+{
+	size_t node_nbr = 1;
+
+	if (h == NULL)
+		return (0);
+
+	while (h->next != NULL)
+	{
+		h = h->next;
+		node_nbr++;
+	}
+	
+	return (node_nbr);
+}
