5fb36f7578b12f2403421ff9acfd322ab893efd5
KouraMouhissin <mouhissin@gmail.com>
2023-04-29 06:30:36 -0700

    if statement ajusted on while line

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -21,11 +21,6 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	if (*head == NULL)
 	{
-		while (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-		}
-		tmp->next = nnde;
 		*head = nnde;
 	}
 	else
