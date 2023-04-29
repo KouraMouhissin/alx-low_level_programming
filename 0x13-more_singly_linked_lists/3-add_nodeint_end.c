db086a61e1a38a7c2b1769b4a60c62d2f482bfe8
KouraMouhissin <mouhissin@gmail.com>
2023-04-29 05:49:12 -0700

    statement corrected

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+/**
+ * add_nodeint_end - new node on tail
+ * @head: lst head
+ * @n: int
+ * Return: address/null
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *nnde;
+	listint_t *tmp;
+
+	(void)tmp;
+
+	nnde = malloc(sizeof(listint_t));
+
+	(*nnde).n = n;
+	(*nnde).next = NULL;
+	tmp = *head;
+
+	if (*head != NULL)
+	{
+		while (tmp->next != NULL)
+		{
+			tmp = tmp->next;
+		}
+		tmp->next = nnde;
+	}
+	else
+	{
+		*head = nnde;
+	}
+
+	if (nnde == NULL)
+		return (NULL);
+
+	return (*head);
+}
