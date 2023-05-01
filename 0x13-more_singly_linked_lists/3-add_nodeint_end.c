f3f658f43cc32724ff154243d62d50b6d9b0400a
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 14:29:32 -0700

    betty correction

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,9 +11,10 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *nnde;
 	listint_t *tmp;
 
-	(void)tmp;
-
 	nnde = malloc(sizeof(listint_t));
+
+	if (nnde == NULL)
+		return (NULL);
 
 	(*nnde).n = n;
 	(*nnde).next = NULL;
@@ -24,14 +25,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		*head = nnde;
 		return (nnde);
 	}
-	while (tmp->next != NULL)
+	while (tmp->next)
 	{
 		tmp = tmp->next;
 	}
 	tmp->next = nnde;
-
-	if (nnde == NULL)
-		return (NULL);
 
 	return (nnde);
 }
