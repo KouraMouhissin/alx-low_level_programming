a2188758917a55a4259971ae1647ebe55b97f5dd
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 06:20:54 -0700

    if replace by while

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,9 +12,7 @@ size_t print_listint(const listint_t *h)
 	if (h != NULL)
 	{
 		printf("%d\n", h->n);
-		h = h->next;
-		nbr_nde++;
-		return (nbr_nde);
+		return (1 + print_listint(h->next));
 	}
 	else
 		return (0);
