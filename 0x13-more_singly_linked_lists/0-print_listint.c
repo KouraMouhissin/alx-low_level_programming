329df7b5e1ba7aec4c9ffffd08c7881a742baf42
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 07:11:43 -0700

    betty fixed

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,8 +11,8 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		printf("%d\n", h->n);
-		return(1 +  print_listint(h->next));
+		printf("%d\n", (*h).n);
+		return (1 +  print_listint((*h).next));
 	}
 
 	return (nbr_nde);
