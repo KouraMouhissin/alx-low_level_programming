18a30af3c8d2e22c9eb97bec6a3ad302a42c1fa8
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 16:15:27 -0700

    Initial commit for the 7 tasks

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -10,11 +10,12 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int n = 0;
 	listint_t *tmp = head;
+
 	while (tmp && n < index)
 	{
 		tmp = tmp->next;
 		n++;
 	}
-	
+
 	return (tmp ? tmp : NULL);
 }
