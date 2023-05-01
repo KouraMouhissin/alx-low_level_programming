8da656dd939f811a80995afaf08f64b9e83496f7
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 14:06:07 -0700

    A little prob with if

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -22,15 +22,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if (*head == NULL)
 	{
 		*head = nnde;
+		return (nnde);
 	}
-	else
+	while (tmp->next != NULL)
 	{
-		while (tmp->next != NULL)
-		{
-			tmp = tmp->next;
-		}
-		tmp->next = nnde;
+		tmp = tmp->next;
 	}
+	tmp->next = nnde;
 
 	if (nnde == NULL)
 		return (NULL);
