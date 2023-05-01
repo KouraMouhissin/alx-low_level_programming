093993c5537922dc0fd5b28fe338b55c20ea4d90
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 16:26:35 -0700

    Initial commit of file 8-sum

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+
+#include "lists.h"
+/**
+ * sum_listint - calculates the sum of all the data in a listint_t list
+ * @head: first node in the linked list
+ * Return: resulting sum
+ */
+
+int sum_listint(listint_t *head)
+{
+	int totl = 0;
+	listint_t *tmp = head;
+
+	while (tmp)
+	{
+		totl+= (*tmp).n;
+		tmp = (*tmp).next;
+	}
+	return (totl);
+}
