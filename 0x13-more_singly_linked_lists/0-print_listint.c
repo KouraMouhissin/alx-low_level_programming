534b440b8b7c49e5f300f617330b48006c0472b8
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 13:54:28 -0700

    Little modif on printf

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,26 +11,9 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		int a, b, c, d;
-
-		a = (*h).n;
-
-		if (a < 10)
-		{
-			putchar(48 + (*h).n);
-			putchar('\n');
-		}
-		else if (a > 10 && a < 100)
-		{
-			b = a % 10;
-			c = a - b;
-			d = c / 10;
-			putchar(48 + d);
-			putchar(48 + b);
-			putchar('\n');
-		}
-
-		return (1 +  print_listint((*h).next));
+		printf("%d\n", (*h).n);
+		nbr_nde++;
+		h = (*h).next;
 	}
 
 	return (nbr_nde);
