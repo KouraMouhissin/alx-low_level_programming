11cfc0160e64593517aad2a80850b5d6fe2a2fc4
KouraMouhissin <mouhissin@gmail.com>
2023-05-01 15:53:27 -0700

    Correct output check 10:wq

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,11 +11,11 @@ int pop_listint(listint_t **head)
 	listint_t *tmp;
 	int i;
 
-	i = (*(*head)).n;
-	tmp = (*(*head)).next;
-
 	if (!head || !*head)
 		return (0);
+
+	i = (*(*head)).n;
+	tmp = (*(*head)).next;
 
 	free(*head);
 	*head = tmp;
