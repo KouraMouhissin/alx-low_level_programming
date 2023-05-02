0fabdebb09d09d628145dff7e7a90824d142208e
KouraMouhissin <mouhissin@gmail.com>
2023-05-02 06:40:49 -0700

    Some changes on pointers declara

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses
+ * @head: ead pointer for lists
+ * Return: a pointer
+ */
+
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *suiv = NULL;
+	listint_t *prec = NULL;
+
+	while (*head)
+	{
+		suiv = (*(*head)).suiv;
+		(*(*head)).suiv = prec;
+		prec = *head;
+		*head = suiv;
+	}
+	*head = prec;
+	return (*head);
+}
