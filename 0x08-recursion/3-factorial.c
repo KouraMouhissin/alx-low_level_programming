b7e6b4477044a79102e694b4fd92f453f2f10386
KouraMouhissin <mouhissin@gmail.com>
2023-06-07 07:05:46 -0700

    Task 4 first commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - function to print factorial of given int
+ * @n: given int
+ * Return: an int/ the factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
