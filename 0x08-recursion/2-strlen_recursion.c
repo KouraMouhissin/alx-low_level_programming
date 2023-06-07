aa8ecb145163262e9675a5e11d88333da541fd85
KouraMouhissin <mouhissin@gmail.com>
2023-06-07 06:59:50 -0700

    Task 2 first commit

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function to print string length
+ * @s: given string
+ * Return: an int/ the string lenght
+ */
+int _strlen_recursion(char* s)
+{
+	if (*s == '\0')
+		return (0);
+	else
+		return (1 + _strlen_recursion(s + 1));
+}
