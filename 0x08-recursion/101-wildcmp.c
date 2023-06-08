9b32f2ce010b9a32778958671ce74d791eb19205
KouraMouhissin <mouhissin@gmail.com>
2023-06-08 07:13:13 -0700

    First commit of this second advanced task

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * wildcmp - function to compare 2 strings
+ * @s1: first string
+ * @s2: second string
+ * Return: an int/ the factorial
+ */
+int wildcmp(char *s1, char *s2)
+{
+
+}
