6d81227253e5fd3383113b1d1611c11e5f5cf189
KouraMouhissin <mouhissin@gmail.com>
2023-06-12 12:56:41 -0700

    1rs commit pf my project

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * create_array - creates chars array
+ * @size: array size
+ * @c: given char
+ * Return: pointer
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *ar;
+	unsigned int i;
+
+	if (size == 0)
+		return (NULL);
+
+	ar = malloc(sizeof(c) * size);
+
+	if (ar == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ar[i] = c;
+	}
+
+	return (ar);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat - print a concat string
+ * @s1: given string 1
+ * @s2: given string 2
+ * Return: s1 followd by s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+	char *out;
+	unsigned int i, j, k, fin;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0; s1[i] != '\0'; i++)
+		;
+	for (j = 0; s2[i] != '\0'; j++)
+		;
+
+	out = malloc(sizeof(char) * (i + j + 1));
+
+	if (out == NULL)
+	{
+		free(out);
+		return (NULL);
+	}
+
+	for (k = 0; k < i; k++)
+		out[k] = s1[k];
+
+	fin = j;
+
+	for (j = 0; j <= fin; k++, j++)
+		out[k] = s2[j];
+
+	return (out);
+}
