eac75106feaa04b30d1c20e8e40f657c28b47ae2
KouraMouhissin <mouhissin@gmail.com>
2023-06-21 12:25:12 -0700

    First push of this adv task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -15,17 +15,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 2)
 	{
+		/*Pour voir si le nbr d'arg est diff de 2*/
 		printf("Error\n");
 		exit(1);
 	}
 	byt = atoi(argv[1]);
 	if (byt < 0)
 	{
+		/*Voir si byt est inferieur à 0*/
 		printf("Error\n");
 		exit(2);
 	}
 	for (i = 0; i < byt; i++)
 	{
+		/*La boocle pour l'iteration*/
 		printf("%02x", ptr[i] & 0xFF);
 		if (i != byt - 1)
 			printf(" ");
