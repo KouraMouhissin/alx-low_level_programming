2ea63b225c476f881542a0fdac9e91499fe42577
KouraMouhissin <mouhissin@gmail.com>
2023-06-22 10:38:55 -0700

    First commit of this task

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,26 @@
+#include "variadic_functions.h"
+/**
+ * print_numbers - nbrs printer
+ * @separator: chaine à afficher entre les nbrs
+ * @n: nbrs d'entier en args.
+ * Return: nothing void
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list lst;
+	unsigned int rec;
+
+	va_start(lst, n);
+
+	for (rec = 0; rec < n; i++)
+	{
+		/*Boocle pour laffichage ds nbres*/
+		printf("%d", va_arg(lst, int));
+		if (separator && rec <= n)
+			printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(lst);
+}
+
